361e9bd35e7765ff12936a41bbe452293344c04b
Ayalni2289 <alininspor@hotmail.com>
2024-02-03 20:27:20 +0300

    Result-price component added.

---
+++ restaurant/components/ResultList.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function ResultList({title,result}) {
+    console.log(result);
+  return (
+    <View>
+      <Text>ResultList</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
--- restaurant/screens/SearchScreen.js
+++ restaurant/screens/SearchScreen.js
@@ -1,16 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import SearchBar from '../components/SearchBar'
-import useResult from '../hooks/useResult'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import SearchBar from "../components/SearchBar";
+import useResult from "../hooks/useResult";
+import ResultList from "../components/ResultList";
 
 export default function SearchScreen() {
-  const [searchApi,result] = useResult();
-  console.log(result);
+  const [searchApi, result] = useResult();
+  
+  const filterResultByPrice = (price) => {
+    return result.filter((result) => {
+      return result.price === price;
+    });
+  };
   return (
     <View>
       <SearchBar />
+      <ResultList
+        title={"Cheap Restaurants"}
+        result={filterResultByPrice("₺")}
+      />
+      <ResultList
+        title={"Medium-Price Restaurants"}
+        result={filterResultByPrice("₺₺")}
+      />
+      <ResultList
+        title={"Expensive Restaurants"}
+        result={filterResultByPrice("₺₺₺")}
+      />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
