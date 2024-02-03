0449baf1de32848b6507ebfaf985e8d37eb68860
Ayalni2289 <alininspor@hotmail.com>
2024-02-03 20:35:49 +0300

    getting restaurant info

--- restaurant/components/ResultList.js
+++ restaurant/components/ResultList.js
@@ -1,13 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList,TouchableOpacity } from 'react-native'
 import React from 'react'
+import ResultDetail from './ResultDetail';
 
 export default function ResultList({title,result}) {
     console.log(result);
   return (
     <View>
-      <Text>ResultList</Text>
+      <Text>{title}</Text>
+      <FlatList
+        data={result}
+        renderItem={({ item }) => {
+          return(
+            <TouchableOpacity>
+              <ResultDetail  result={item}/>
+            </TouchableOpacity>
+          )
+        }}
+        />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({})
