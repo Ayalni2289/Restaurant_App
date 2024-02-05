f484793c4cead5ed177adf671a89fd987c70eeb1
Ayalni2289 <alininspor@hotmail.com>
2024-02-05 17:04:43 +0300

    fetching restaurants informations by id's

--- restaurant/screens/ResultShowScreen.js
+++ restaurant/screens/ResultShowScreen.js
@@ -1,12 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import yelp from "../api/yelp";
 
-export default function ResultShowScreen() {
+export default function ResultShowScreen({ route }) {
+  const id = route.params.id;
+  const [result, setResult] = useState(null);
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+  if (!result) {
+    return null;
+  }
+
   return (
     <View>
-      <Text>ResultShowScreen</Text>
+      <Text>{result.name}</Text>
+      <Text>{result.phone}</Text>
+      <Text>{result.location.address1}</Text>
+      <Text>{result.location.city}</Text>
+      <FlatList
+        data={result.photos}
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />;
+        }}
+      />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300,
+  },
+});
